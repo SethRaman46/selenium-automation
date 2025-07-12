d578b8f548f1f4dcac744518559ee80f6db9e6ce
SethuRaman46 <sethukannah46@gmail.com>
2025-07-12 08:44:00 +0530

    changes made to master

---
+++ src/ChromeDriverdemo/OpenChrome1.java
@@ -0,0 +1,18 @@
+package ChromeDriverdemo;
+
+public class OpenChrome1 {
+
+
+
+
+
+		public static void main(String[] args) {
+			
+			
+//			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\seleniumfundamental\\Drivers\\chromedriver.exe");
+//			WebDriver driver = new ChromeDriver();
+//			driver.get("https://www.google.com");
+			
+}
+		
+}
